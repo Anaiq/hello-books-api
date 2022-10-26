c26473a536a6179216f3009a74d3e129043eb039
Anaiq <>
2022-10-26 11:26:08 -0500

    defined book Model in book.py, connected db to flask in __init__.py

---
+++ app/models/book.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    year_published = db.Column(db.Integer)
+    description = db.Column(db.String)
