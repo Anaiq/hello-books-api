5b6b84758f9a8092e6ee55e78fbb6cd632a13673
Anaiq <>
2022-11-04 11:25:36 -0500

    created test for post, refactored routes.py

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,7 @@ def create_book():
     db.session.add(new_book)
     db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    return make_response(jsonify(f"Book {new_book.title} successfully created"), 201)
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
@@ -50,6 +50,7 @@ def read_all_books():
         )
     return jsonify(books_response)
 
+
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
     book = validate_book(book_id)
@@ -70,7 +71,7 @@ def update_book(book_id):
 
     db.session.commit()
 
-    return make_response(f"Book #{book.id} successfully updated")
+    return make_response(jsonify(f"Book #{book.id} successfully updated"))
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_book(book_id):
@@ -79,7 +80,7 @@ def delete_book(book_id):
     db.session.delete(book)
     db.session.commit()
 
-    return make_response(f"Book #{book.id} successfully deleted")
+    return make_response(jsonify(f"Book #{book.id} successfully deleted"))
 
 
 
