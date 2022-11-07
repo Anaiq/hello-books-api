9c5692b2252845537cf51dd1663d2b1280261aa2
Anaiq <>
2022-11-06 20:03:18 -0600

    added nested routes to post a book related to an author and get all book by an author

--- app/author_routes.py
+++ app/author_routes.py
@@ -1,21 +1,23 @@
 from app import db
 from app.models.author import Author
+from app.models.book import Book
+from app.book_routes import validate_model
 from flask import Blueprint, jsonify, abort, make_response, request
 
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
 
-def validate_model(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+# def validate_model(cls, model_id):
+#     try:
+#         model_id = int(model_id)
+#     except:
+#         abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
 
-    model = cls.query.get(model_id)
+#     model = cls.query.get(model_id)
 
-    if not model:
-        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+#     if not model:
+#         abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
 
-    return model
+#     return model
 
 @authors_bp.route("", methods=["POST"])
 def create_author():
@@ -26,6 +28,24 @@ def create_author():
     db.session.commit()
 
     return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
+
+#send a request to create a new book and connect it to an author already found in the database
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book_associated_with_author(author_id):
+    author = validate_model(Author, author_id)
+
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body['title'],
+        description=request_body["description"],
+        author=author
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
 
 @authors_bp.route("", methods=["GET"])
 def read_all_authors():
@@ -40,6 +60,17 @@ def read_all_authors():
         authors_response.append(author.to_dict())
 
     return jsonify(authors_response)
+
+
+#send a request to read all books by a particular author in the database.
+@authors_bp.route("/<author_id>/books", methods=["GET"])
+def read_all_books_from_author(author_id):
+    author = validate_model(Author, author_id)
+
+    books_response = []
+    for book in author.books:
+        books_response.append(book.to_dict())
+    return jsonify(books_response)
 
 @authors_bp.route("/<author_id>", methods=["GET"])
 def read_one_author(author_id):
--- app/book_routes.py
+++ app/book_routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
